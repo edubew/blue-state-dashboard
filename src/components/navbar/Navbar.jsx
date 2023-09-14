270d9ad480b5d7c6585d5f2e3b22ffe7ce8dd412
edubew <edubew98@gmail.com>
2023-09-14 22:56:10 +0300

    Add react icons to the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,30 @@
 import "./navbar.scss";
+import { BiSearchAlt } from "react-icons/bi";
+import { AiOutlineAppstore, AiOutlineExpand } from "react-icons/ai";
+import { IoMdNotifications } from "react-icons/io";
+import { MdSettingsSuggest } from "react-icons/md";
+import user from "../../assets/user.jpg";
 
 const Navbar = () => {
   return (
-    <div>
-      <h2>Navbar</h2>
+    <div className="navbar__container">
+      <div className="logo">
+        <h2>Blue State</h2>
+      </div>
+      <div className="navbar__icons">
+        <BiSearchAlt className="icon" />
+        <AiOutlineAppstore className="icon" />
+        <AiOutlineExpand className="icon" />
+        <div className="notifications">
+          <IoMdNotifications className="icon" />
+          <span>1</span>
+        </div>
+        <div className="user__container">
+          <img src={user} alt="profile" />
+          <span>Edube</span>
+        </div>
+        <MdSettingsSuggest className="icon" />
+      </div>
     </div>
   )
 }

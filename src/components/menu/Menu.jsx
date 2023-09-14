db9b26c11c13bdd1e11512a9597230c6eeee3805
edubew <edubew98@gmail.com>
2023-09-15 02:34:28 +0300

    Add side menu items and styling

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,11 +1,23 @@
 import "./menu.scss";
+import { Link } from "react-router-dom";
+import { menu } from "../../data";
 
 const Menu = () => {
   return (
-    <div>
-      <h2>Menu</h2>
+    <div className="side__menu">
+      {menu.map((item) => (
+        <div className="menu__item" key={item.id}>
+          <span className="title">{item.title}</span>
+          {item.listItems.map((listItem) => (
+            <Link to={listItem.url} className="list__item" key={listItem.id}>
+              {listItem.icon()}
+              <span className="list__title">{listItem.title}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Menu
+export default Menu;

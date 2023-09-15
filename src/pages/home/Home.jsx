2cdd9ba27d10922360dc7d7376694d696f1f3dcc
edubew <edubew98@gmail.com>
2023-09-15 03:05:39 +0300

    Write a grid markup for the home components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,8 +2,16 @@ import "./home.scss";
 
 const Home = () => {
   return (
-    <div>
-      <h2>Homepage</h2>
+    <div className="home__container">
+      <div className="box__container box1">Box1</div>
+      <div className="box__container box2">Box2</div>
+      <div className="box__container box3">Box3</div>
+      <div className="box__container box4">Box4</div>
+      <div className="box__container box5">Box5</div>
+      <div className="box__container box6">Box6</div>
+      <div className="box__container box7">Box7</div>
+      <div className="box__container box8">Box8</div>
+      <div className="box__container box9">Box9</div>
     </div>
   )
 }

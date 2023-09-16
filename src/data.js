1b9a317d029b53c4f614aaed00427fa4cb2b1cdc
edubew <edubew98@gmail.com>
2023-09-16 14:33:09 +0300

    Add dummy data for user, revenue, products and conversion charts

--- src/data.js
+++ src/data.js
@@ -165,4 +165,72 @@ export const transactions = [
         email: "edube@gmail.com",
         amount: "4.25",
     },
-]
+];
+
+export const chartBoxUser = {
+    color: "#8884d8",
+    title: "Total Users",
+    number: "11.238",
+    dataKey: "users",
+    percentage: 45,
+    chartData: [
+        { name: "Sun", users: 400 },
+        { name: "Mon", users: 600 },
+        { name: "Tue", users: 500 },
+        { name: "Wed", users: 700 },
+        { name: "Thu", users: 400 },
+        { name: "Fri", users: 500 },
+        { name: "Sat", users: 450 },
+    ],
+};
+
+export const chartBoxProduct = {
+    color: "skyblue",
+    title: "Total Products",
+    number: "238",
+    dataKey: "products",
+    percentage: 21,
+    chartData: [
+        { name: "Sun", products: 400 },
+        { name: "Mon", products: 600 },
+        { name: "Tue", products: 500 },
+        { name: "Wed", products: 700 },
+        { name: "Thu", products: 400 },
+        { name: "Fri", products: 500 },
+        { name: "Sat", products: 450 },
+    ],
+};
+
+export const chartBoxRevenue = {
+    color: "teal",
+    title: "Total Revenue",
+    number: "$56.238",
+    dataKey: "revenue",
+    percentage: -12,
+    chartData: [
+        { name: "Sun", revenue: 400 },
+        { name: "Mon", revenue: 600 },
+        { name: "Tue", revenue: 500 },
+        { name: "Wed", revenue: 700 },
+        { name: "Thu", revenue: 400 },
+        { name: "Fri", revenue: 500 },
+        { name: "Sat", revenue: 450 },
+    ],
+};
+
+export const chartBoxConversion = {
+    color: "gold",
+    title: "Total Ratio",
+    number: "2.6",
+    dataKey: "ratio",
+    percentage: 15,
+    chartData: [
+        { name: "Sun", ratio: 400 },
+        { name: "Mon", ratio: 600 },
+        { name: "Tue", ratio: 500 },
+        { name: "Wed", ratio: 700 },
+        { name: "Thu", ratio: 400 },
+        { name: "Fri", ratio: 500 },
+        { name: "Sat", ratio: 450 },
+    ],
+};
